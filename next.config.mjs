ce883fdbd36b998311dddeade1d4cd57ef621dc4
Manoranjanhere <manoranjanheremano@gmail.com>
2025-01-07 16:10:21 +0530

    first commit

---
+++ next.config.mjs
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images: {
+        domains: ['images.unsplash.com', 'res.cloudinary.com', 'cdn.iconscout.com', 'media.licdn.com']
+    }
+};
+
+export default nextConfig;
